cdc2a9c1fea51def88c4916e42e778710f418a72
John Sichi <jsichi@gmail.com>
2020-12-26 19:19:05 +0900

    Update to match extjwnl-data-mcr30 lang-spa module rename

--- extjwnl/updateExtjwnl.sh
+++ extjwnl/updateExtjwnl.sh
@@ -6,12 +6,11 @@ BIN_DIR_REL=`dirname "${BASH_SOURCE[0]}"`
 BIN_DIR=`readlink -f ${BIN_DIR_REL}`
 
 MCR30_DIR=${BIN_DIR}/../../extjwnl-data-mcr30
-SPA_DIR=${MCR30_DIR}/data-spa/src/main/resources/net/sf/extjwnl/data/mcr30/spa
+SPA_DIR=${MCR30_DIR}/lang-spa/src/main/resources/net/sf/extjwnl/data/mcr30/spa
+ALIGN_DIR=${MCR30_DIR}/alignment/src/main/resources/net/sf/extjwnl/data/mcr30/alignment
 
 MAPPING_JSON=${BIN_DIR}/mapping_wordnet.json
-ILI_CSV=${SPA_DIR}/ili.csv
-MAPPING_CSV=${BIN_DIR}/mapping.csv
-RESULT_CSV=${SPA_DIR}/translation.csv
+ALIGN_CSV=${ALIGN_DIR}/wn31-30.csv
 
 pushd ${BIN_DIR}
 wget https://github.com/ozendelait/wordnet-to-json/raw/master/mapping_wordnet.json
@@ -19,7 +18,7 @@ wget https://sourceforge.net/projects/extjwnl/files/extjwnl-2.0.3.zip
 unzip extjwnl-2.0.3.zip
 cp ${SPA_DIR}/res_properties.xml ./res_properties.xml
 cp ./res_properties.xml ./ewn.xml
-sed -i "s|/net|${MCR30_DIR}/data-spa/src/main/resources/net|g" ewn.xml
+sed -i "s|/net|${MCR30_DIR}/lang-spa/src/main/resources/net|g" ewn.xml
 sed -i "s|PrincetonResourceDictionaryFile|PrincetonRandomAccessDictionaryFile|g" ewn.xml
 wget https://github.com/lingeringsocket/wn-mcr-transform/archive/master.zip
 unzip master.zip wn-mcr-transform-master/wordnet_spa.tar.gz
@@ -30,12 +29,6 @@ rm -f README.txt
 cp ${BIN_DIR}/res_properties.xml ./res_properties.xml
 popd
 extjwnl-2.0.3/bin/ewn -script supplemental_spa.txt
-awk '/synset-mapping/{flag=1;next}/synset-missing/{exit}flag' ${MAPPING_JSON} | head -n -1 | tr -s '":, ' ' ' | awk '{$1=$1};1' | sort -k 2 | tr -s ' ' ',' > ${MAPPING_CSV} 
-
-# result
-# first column: 0-based Spanish synset line number
-# second column: synset offset in Princeton WordNet 3.1
-# third column: synset offset in Princeton WordNet 3.0
-sort ${ILI_CSV} | join -t ',' -j 2 -o '1.1','2.1','1.2' --check-order - ${MAPPING_CSV} > ${RESULT_CSV}
-rm -rf master.zip wn-mcr-transform-master extjwnl-2.0.3.zip extjwnl-2.0.3 ewn.xml ${MAPPING_CSV} ${ILI_CSV} ${MAPPING_JSON} res_properties.xml
+awk '/synset-mapping/{flag=1;next}/synset-missing/{exit}flag' ${MAPPING_JSON} | head -n -1 | tr -s '":, ' ' ' | awk '{$1=$1};1' | sort -k 2 | tr -s ' ' ',' > ${ALIGN_CSV}
+rm -rf master.zip wn-mcr-transform-master extjwnl-2.0.3.zip extjwnl-2.0.3 ewn.xml ${MAPPING_JSON} res_properties.xml
 popd
